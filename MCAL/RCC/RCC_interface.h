c4e786d4b58f334cb47344a9e15a46e6de4e14ec
Shaimaa G.Salem <sgamal@tmcmf.com>
2023-11-02 22:07:21 +0200

    add rcc driver

--- MCAL/RCC/RCC_interface.h
+++ MCAL/RCC/RCC_interface.h
@@ -11,5 +11,12 @@
 #define RCC_APB1  1
 #define RCC_APB2  2
 
+
+
+void RCC_voidInitSysClock(void);
+
+void RCC_voidEnableClock(u8 Copy_u8BusId, u8 Copy_u8PerId);
+
+void RCC_voidDisableClock(u8 Copy_u8BusId, u8 Copy_u8PerId);
 #endif /*RCC_INTERFACE_H*/
 
